9dcfcea8aaf4c0c158d45159d72f9e96ccd45b4c
Michael Wenhold <michael@Michaels-MacBook-Pro.local>
2018-08-19 20:09:43 -0500

    unit-4-game

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+//Pseudo Code:
+//There are 4 crystals displayed as buttons
+//Generate a random number at start of game for player between 19-120
+//Player clicks on a crystal, it will add a specific amount of points to palyers total (hidden values change for each game)
+//The amount is hidden until the crystal is clicked (hidden value between 1-12)
+//Player's score is updated after each click
+//Player wins if their score matches random number
+//Player loses if their score goes over random number
+//Game resets after each win/lose
+//Page shows wins & losses total
